1a3a68382dc4ec4b52abc692995d795ac1042f13
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-05-06 17:05:29 +0100

    Calculator works for positive numbers

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -39,4 +39,12 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('13')
   })
+  it('should work for positive numbers', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number7')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('15')
+  })
 });
