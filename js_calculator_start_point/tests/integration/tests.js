afcb8f28958a8cb4276f0380658639c7cbd160ab
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-05-06 16:59:42 +0100

    working number button tested

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -8,4 +8,10 @@ describe('calculator functionality', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://localhost:3000');
   });
+  it('should have working number buttons', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    const runningTotalValue = running_total.getAttribute('value')
+    expect(runningTotalValue).to.eventually.equal('2')
+  });
 });
