525b47ade0b3d1be3a7afb533ea28fffa96c4ccd
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-05-06 16:50:28 +0100

    Test for adding done and working

---
+++ js_calculator_start_point/tests/integration/tests.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
+describe('calculator functionality', function() {
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://localhost:3000');
+  });
+});
---
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -0,0 +1,19 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+  it('it should adds 1 to 4 and get 5', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    actual = calculator.runningTotal;
+    assert.strictEqual(actual, 5);
+  })
+});
